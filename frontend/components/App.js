877ddf31b7961c12037cc92b384c81604f922727
jamieellen <jamieellen.w@gmail.com>
2023-09-27 13:17:40 -0700

    counterintuitive but can see benefits

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,43 @@
 import React from 'react'
+import Form from './Form'
+import TodoList from './TodoList'
+
+let id = 0;
+let getId = () => ++id;
+
+const initialToDos = [
+  { id: getId(), name: 'Design & Complete 1st App', completed: false },
+  { id: getId(), name: 'Play w/ Tigger & BudBud', completed: true },
+  { id: getId(), name: 'Meditate', completed: false },
+]
 
 export default class App extends React.Component {
+  state = {
+    todos: initialToDos
+  }
+
+  addToDo = (name) => {
+    this.setState ({
+      ...this.state,
+      todos: this.state.todos.concat({ id: getId(), completed: false, name })
+    })
+  }
+
+  toggleCompletion = id => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.map(td => {
+        if( id ==  td.id ) return {...td, completed: !td.completed}
+        return td;
+      })
+    })
+  }
+
   render() {
     return (
       <div>
-        Todo App
+        <TodoList todos={this.state.todos} toggleCompletion={this.toggleCompletion}/>
+        <Form addToDo={this.addToDo}/>
       </div>
     )
   }
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,11 +1,37 @@
 import React from 'react'
 
 export default class Form extends React.Component {
+  state = {
+    name: '',
+  }
+
+  onSubmit = evt => {
+    evt.preventDefault()
+    this.props.addToDo(this.state.name)
+    //COULD ALSO LOOK LIKE
+    // const {addToDo} = this.props
+    // addToDo(this.state.name)
+    this.setState({
+      ...this.state,
+      name: ''
+    })
+  }
+
+  onChange = evt => {
+    const {value} = evt.target
+    this.setState({
+      ...this.state,
+      name: value
+    })
+  }
+
   render() {
     return (
-      <div>
-        Form
-      </div>
+      <form onSubmit={this.onSubmit}>
+        {/* <button onClick={evt => addToDo('Write Letter')}> Add </button> */}
+        <input type='text' value={this.state.name} onChange={this.onChange}></input>
+        <input type='submit' ></input>
+      </form>
     )
   }
 }
